442ff2983e0ca000bd6982cc89a63bde42b3387b
borolo222 <35582191+borolo222@users.noreply.github.com>
2022-02-07 21:33:20 -0500

    script change

--- rooteb.sh
+++ rooteb.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 
-# Download netselect to choose best mirror.
-apt install netselect-apt -y
-
 # Change Debian to SID Branch
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
-cp sources.list /etc/apt/sources.list 
-
-
-username=$(id -u -n 1000)
-builddir=$(pwd)
-
-# Add Custom Titus Rofi Deb Package
-#dpkg -i 'Custom Packages/rofi_1.7.0-1_amd64.deb'
-
-# move to /etc/apt and run netselect
-cd /etc/apt
-netselect sid -c US
-cd builddir
+cp build/sources.list /etc/apt/sources.list 
 
 # Update packages list
 apt update
 
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+# move to /etc/apt and run netselect
+#cd /etc/apt
+#netselect-apt sid -c US
+cd builddir || exit
+
 # Add base packages
-apt install unzip picom bspwm polybar sddm rofi terminator thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg -y
+apt install unzip picom bspwm polybar sddm rofi terminator thunar flameshot neofetch sxhkd git lxpolkit lxappearance xorg arandr netselect-apt -y
 apt install libqt5svg5 qml-module-qtquick-controls papirus-icon-theme feh -y
 apt install pavucontrol psmisc gucharmap dunst yad libnotify-bin playerctl -y
-apt install qemu-guest-agent apt-transport-https curl colortest xprop nfs-common -y
+apt install qemu-guest-agent apt-transport-https curl colortest nfs-common -y
 apt install neovim cifs-utils kcharselect htop wmctrl graphicsmagick virt-manager lm-sensors -y
 
-#apt install fonts-noto-color-emoji fonts-firacode fonts-font-awesome -y
 #Install I3lock and dependencies
-apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
+apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util0-dev libxcb-xrm-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev -y
 
 # Download i3lock-color
 git clone https://github.com/Raymo111/i3lock-color.git
@@ -41,16 +33,22 @@ git clone https://github.com/Raymo111/i3lock-color.git
 
 
 # Download Nordic Theme
-cd /usr/share/themes/
+cd /usr/share/themes/ || exit
 git clone https://github.com/EliverLara/Nordic.git
 
 # Fira Code Nerd Font variant needed
-cd $builddir
+cd "$builddir" || exit
+
 #wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/FiraCode.zip
-#unzip FiraCode.zip -d /usr/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
+unzip FiraCode.zip -d /usr/share/fonts
 #wget https://github.com/ryanoasis/nerd-fonts/releases/download/v1.1.0/Meslo.zip
 #unzip Meslo.zip -d /usr/share/fonts
-#fc-cache -vf
+fc-cache -vf
+
+#Install Starship
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
 
 # install brave
 apt install apt-transport-https curl -y
@@ -70,19 +68,38 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
 apt update
 apt install codium -y
 
-cd $builddir
-mkdir -p /home/$username/.config
-mkdir -p /home/$username/.local
+
+# Copy config files
+
+cd "$builddir" || exit
+mkdir -p /home/"$username"/.config
+mkdir -p /home/"$username"/.local
 
 #cp .Xresources /home/$username
 #cp .Xnord /home/$username
-cp .bashrc /home/$username
-cp .profile /home/$username
-cp .fehhbg /home/$username
-cp .dir_colors /home/$username
-cp .fonts.conf /home/$username
-source .bashrc
+cp .bashrc /home/"$username"
+cp .profile /home/"$username"
+cp .fehbg /home/"$username"
+cp .dir_colors /home/"$username"
+cp .fonts.conf /home/"$username"
 
-cp -R .config/* /home/$username/.config/
-cp -R .local/* /home/$username/.local/
-chown -R $username:$username /home/$username
+cp -R .config/* /home/"$username"/.config/
+cp -R .local/* /home/"$username"/.local/
+chown -R "$username":"$username" /home/"$username"
+
+
+### Additional steps for VMS
+## Polybar
+# - Change the monitor
+# - Remove module fan-speed
+# - Change the ETHO name
+
+## Resolution
+# run arandr
+# Select resolution and save as def.sh
+# Insert at end of .profile file:
+# bash ~/.screenlayout/def.sh
+
+## Pycom
+# change vsync to false
+
